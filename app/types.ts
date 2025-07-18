3c70dc49971834a7aa633a1b8736f2706b70ce82
Muhammad Rizki <kiizuha@gnuweeb.org>
2025-07-18 14:17:58 +0700

    feat(types): add type interfaces
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>

---
+++ app/types.ts
@@ -0,0 +1,11 @@
+export interface SectionProps {
+  delay: number;
+  style: React.CSSProperties;
+}
+
+export interface ProjectData {
+  title: string;
+  description: string;
+  content: string;
+  url?: string;
+}
