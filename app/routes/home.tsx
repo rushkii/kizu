18996e538d158803bdb737b63518377b2191991b
Kiizuha Kanazawa <kiizuha@gnuweeb.org>
2025-07-20 09:30:07 +0700

    chore(app): no need to loading in dev mode
    Signed-off-by: Kiizuha Kanazawa <kiizuha@gnuweeb.org>

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -16,7 +16,7 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  const [loaded, setLoaded] = useState(true);
+  const [loaded, setLoaded] = useState(import.meta.env.DEV || false);
 
   useEffect(() => {
     if (loaded) return;
