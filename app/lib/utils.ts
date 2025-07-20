afdb38e878b74b779ed5821549c329ccc6f1f9cd
Kiizuha Kanazawa <kiizuha@gnuweeb.org>
2025-07-20 08:27:40 +0700

    feat(utils): add some helper functions
    Signed-off-by: Kiizuha Kanazawa <kiizuha@gnuweeb.org>

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,10 +1,45 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import type { Types } from "use-lanyard";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function getDiscordAvatar(userId?: string, hash?: string | null) {
-  return `https://cdn.discordapp.com/avatars/${userId}/${hash}.png`;
+export function getDiscordAvatar(user?: Types.DiscordUser) {
+  if (!user) return "";
+  return `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`;
+}
+
+export function getInitialName(user?: Types.DiscordUser) {
+  if (!user) return "";
+  const name = user.global_name ?? "";
+  return name
+    .split(" ")
+    .map(function (item) {
+      return item[0];
+    })
+    .join("");
+}
+
+export function getDiscordAssetUrl(appId?: string, assetId?: string) {
+  if (!appId || !assetId) return "";
+  return ddgImgProxy(`https://cdn.discordapp.com/app-assets/${appId}/${assetId}.png`);
+}
+
+export function getAssetUrl(value?: string | null) {
+  if (!value) return "";
+
+  if (value.startsWith("http://") || value.startsWith("https://")) {
+    return ddgImgProxy(value);
+  }
+
+  const match = value.match(/^mp:external\/[^\/]+\/(.+)/);
+  if (!match) return "";
+
+  return ddgImgProxy(match[1].replace(/^(https?)\//, "$1://"));
+}
+
+export function ddgImgProxy(src: string) {
+  return "https://external-content.duckduckgo.com/iu/?u=" + src;
 }
