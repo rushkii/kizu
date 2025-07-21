c3c93db90f7ebd1d4d6e734e30f0b8ea50bcfe6e
Muhammad Rizki <kiizuha@gnuweeb.org>
2025-07-22 00:05:08 +0700

    feat(components): add and update some UI related to activity section
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>

--- app/components/customs/presence-activity.tsx
+++ app/components/customs/presence-activity.tsx
@@ -1,10 +1,21 @@
 import { type Types } from "use-lanyard";
-import { Avatar, AvatarFallback, AvatarImage } from "~/components/magicui/avatar";
-import { Card, CardContent } from "~/components/magicui/card";
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "~/components/magicui/avatar";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "~/components/magicui/card";
 import { ddgImgProxy, getAssetUrl, getDiscordAssetUrl } from "~/lib/utils";
 import { BlurFade } from "~/components/magicui/blur-fade";
 import { useGlobalTimer } from "~/hooks/use-duration";
+import { IconSpotify } from "~/components/icons/icon-spotify";
 import { useMemo } from "react";
+import { IconYtMusic } from "../icons/icon-yt-music";
 
 interface Props {
   index: number;
@@ -16,15 +27,41 @@ export function PresenceActivity({ index, presence, activity }: Props) {
   const currentTime = useGlobalTimer();
 
   const getIconUrl = () => {
-    if (presence?.listening_to_spotify && activity.type === 2) {
+    if (isSpotify()) {
       return ddgImgProxy(presence?.spotify?.album_art_url ?? "");
     }
 
     if (activity.type === 0 && !isNaN(+(activity.assets?.large_image ?? ""))) {
-      return getDiscordAssetUrl(activity.application_id, activity.assets?.large_image);
+      return getDiscordAssetUrl(
+        activity.application_id,
+        activity.assets?.large_image
+      );
     }
 
     return getAssetUrl(activity.assets?.large_image);
+  };
+
+  const getActivityLabel = () => {
+    // https://discord.com/developers/docs/events/gateway-events#activity-object-activity-types
+    switch (activity.type) {
+      case 0:
+        return "Playing";
+      case 1:
+        return "Streaming";
+      case 2:
+        const name = isSpotify() ? "Spotify" : activity.state;
+        return "Listening to " + name;
+      case 3:
+        return "Watching";
+      case 4:
+        return "Custom";
+      case 5:
+        return "Competing";
+    }
+  };
+
+  const isSpotify = () => {
+    return activity.type === 2 && activity.id == "spotify:1";
   };
 
   const duration = useMemo(() => {
@@ -41,35 +78,53 @@ export function PresenceActivity({ index, presence, activity }: Props) {
       durationStr += `${hours}:`;
     }
 
-    durationStr += `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+    durationStr += `${String(minutes).padStart(2, "0")}:${String(
+      seconds
+    ).padStart(2, "0")}`;
     return durationStr;
   }, [currentTime, activity.timestamps?.start]);
 
   return (
     <BlurFade delay={0.25 * index} inView>
       <Card
-        key={index}
-        data-card-index={index}
-        className={`transition-transform duration-300 ease-in-out cursor-pointer w-[calc(100%-2.5rem)] lg:w-[calc(100%-5rem)] h-20 max-h-20 mx-5 my-2 p-2 bg-accent shadow-none border-border
-          ${index === 0 ? "z-30" : index === 1 ? "z-20" : "z-10"}
-          hover:scale-[103%]
+        className={`transition-transform duration-300 ease-in-out gap-0 w-[calc(100%-2.5rem)] lg:w-[calc(100%-5rem)] h-fit
+          mx-5 p-2 bg-accent shadow-none border-border hover:scale-[103%] my-2
       `}
       >
+        <CardHeader className="px-0">
+          <CardTitle className="flex items-center gap-1 font-extrabold text-sm">
+            <span>{getActivityLabel()} </span>
+            {isSpotify() && (
+              <IconSpotify className="text-muted-foreground size-4" />
+            )}
+            {!isSpotify() && activity.type === 2 && (
+              <IconYtMusic className="text-muted-foreground size-4" />
+            )}
+          </CardTitle>
+        </CardHeader>
         <CardContent className="flex gap-2 p-0">
           <Avatar className="rounded-lg w-[4rem] h-full">
             <AvatarImage src={getIconUrl()} className="object-cover" />
-            <AvatarFallback className="bg-transparent">App Icon</AvatarFallback>
+            <AvatarFallback className="bg-transparent text-xs">
+              App Icon
+            </AvatarFallback>
           </Avatar>
           <div className="flex flex-col leading-snug text-xs">
-            <h2 className="font-extrabold uppercase line-clamp-1">{activity.name}</h2>
+            <h2 className="font-extrabold line-clamp-1 text-sm">
+              {activity.name}
+            </h2>
             <div
               title={activity.details}
-              className={`${activity.type === 2 && "font-extrabold"} leading-tight line-clamp-1`}
+              className={`${
+                activity.type === 2 && "font-extrabold"
+              } leading-tight line-clamp-1`}
             >
               {activity.details}
             </div>
-
-            <div title={activity.state} className="leading-tight line-clamp-1 force-break">
+            <div
+              title={activity.state}
+              className="leading-tight line-clamp-1 force-break"
+            >
               {activity.state}
             </div>
             <div className="text-green-500 font-extrabold">{duration}</div>
--- app/components/sections/activity-section.tsx
+++ app/components/sections/activity-section.tsx
@@ -1,6 +1,10 @@
 import { useEffect, useRef, useState } from "react";
 import { type Types, useLanyardWS } from "use-lanyard";
-import { Avatar, AvatarFallback, AvatarImage } from "~/components/magicui/avatar";
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "~/components/magicui/avatar";
 import { ScrollArea } from "~/components/magicui/scroll-area";
 import { SectionWindow } from "~/components/customs/section-window";
 import { DiscordIconIdle } from "~/components/icons/discord/icon-idle";
@@ -58,34 +62,47 @@ export function ActivitySection({ delay, className, style }: SectionProps) {
                 src={getDiscordAvatar(presence?.discord_user)}
                 className="object-cover"
               />
-              <AvatarFallback>{getInitialName(presence?.discord_user)}</AvatarFallback>
+              <AvatarFallback>
+                {getInitialName(presence?.discord_user)}
+              </AvatarFallback>
             </Avatar>
-            <div className="absolute -right-0.5 bottom-0 ">{renderDiscordIcon()}</div>
+            <div className="absolute -right-0.5 bottom-0 ">
+              {renderDiscordIcon()}
+            </div>
           </div>
           <div className="flex flex-col">
-            <div className="text-lg font-extrabold">{presence?.discord_user.global_name}</div>
+            <div className="text-lg font-extrabold">
+              {presence?.discord_user.global_name}
+            </div>
             <div className="text-sm">{presence?.discord_user.username}</div>
           </div>
         </div>
         <div className="flex flex-col">
-          <h3 className="text-xs font-extrabold uppercase mx-5">Activities</h3>
-          {activities.length > 0 ? (
-            <ScrollArea
-              ref={activityRef}
-              className="h-[20rem] relative flex flex-col justify-center"
-            >
-              {activities.map((activity, index) => (
-                <PresenceActivity
-                  key={`activity-${activity.state}-${index}`}
-                  index={index}
-                  presence={presence}
-                  activity={activity}
-                />
-              ))}
-            </ScrollArea>
-          ) : (
-            <div className="italic ml-5 mt-2 text-neutral-400">He is currently offline :)</div>
-          )}
+          <h3 className="text-xs font-extrabold uppercase mx-5 pb-2">
+            Activities
+          </h3>
+          <ScrollArea
+            ref={activityRef}
+            className="h-[20rem] relative flex flex-col justify-center [&>div>div>div>div:nth-child(n+3)]"
+          >
+            {activities.map((activity, index) => (
+              <PresenceActivity
+                key={`activity-${activity.state}-${index}`}
+                index={index}
+                presence={presence}
+                activity={activity}
+              />
+            ))}
+            <div className="italic ml-5 mt-2 text-neutral-400">
+              {presence?.discord_status === "offline" && (
+                <span>He is currently offline</span>
+              )}
+              {presence?.discord_status !== "offline" &&
+                activities.length === 0 && (
+                  <span>He is online, but no activities, probably idle</span>
+                )}
+            </div>
+          </ScrollArea>
         </div>
       </div>
     </SectionWindow>
